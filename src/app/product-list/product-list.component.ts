7e46b39f1e43990bb47a1ff2045369eae079af9a
eanton71 <eanton71@gmail.com>
2022-12-16 18:57:23 +0100

    falta put

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,47 +10,58 @@ import { ProductService } from '../services/product.service';
 })
 export class ProductListComponent {
 
-  products:Product[]=[];
-  productForm:FormGroup;
+  products: Product[] = [];
+  productForm: FormGroup;
 
-  constructor(private productService:ProductService,private fb:FormBuilder){
+  constructor(private productService: ProductService, private fb: FormBuilder) {
     this.productForm = this.fb.group({
-      name:['',Validators.required],
-      price:[0,Validators.required],
-      description:['']
+      name: ['', Validators.required],
+      price: [0, Validators.required],
+      description: ['']
     })
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.getProducts();
   }
 
-  private getProducts():void{
-    this.productService.getProducts().subscribe(res=>this.products = res);
+  private getProducts(): void {
+    this.productService.getProducts().subscribe(res => this.products = res);
   }
 
-  
-  add():void{
 
-    const {name,price,description} = this.productForm.getRawValue();
+  add(): void {
+
+    const { name, price, description } = this.productForm.getRawValue();
 
     this.productForm.reset();
-    
-    this.productService.addNewProduct(name,price,description).subscribe(result=>{
-      
-      if(result){
+
+    this.productService.addNewProduct(name, price, description).subscribe(result => {
+
+      if (result) {
         this.getProducts();
       }
     })
 
   }
 
-  deleteProduct(index:number):void{
-    this.productService.deleteProduct(this.products[index]._id).subscribe(result=>{
-
-      if(result){
+  deleteProduct(index: number): void {
+    this.productService.deleteProduct(this.products[index]._id).subscribe(result => {
+      if (result) {
         this.getProducts();
       }
+    })
+  }
+  updateProduct(index: number): void {
+
+    console.log("src/app/product-list/product-list.component.ts, update product ", index)
+    const { name, price, description } = this.productForm.getRawValue();
+    this.productService.updateProduct(this.products[index]._id, name, price, description).subscribe(result => {
+      /*
+      implementar mensaje que indique se que actuzalizo la inforamcion
+      if (result) {
+        this.getProducts();
+      }*/
     })
   }
 
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -11,6 +11,7 @@ export class ProductService {
   private url = 'http://localhost:3000/api/get_products';
   private urlpost = 'http://localhost:3000/api/add_product';
   private urldelete = 'http://localhost:3000/api/delete_product';
+  private urlput = 'http://localhost:3000/api/put_product';
   constructor(private httpClient:HttpClient) { }
 
   getProducts():Observable<Product[]>{
@@ -25,10 +26,16 @@ export class ProductService {
   }
 
   deleteProduct(id:string):Observable<object>{
-
+    console.log("src/app/services/product.service.ts, deleteProduct(id: string, name: string, price: number, description: string) ",id);
+    console.log("HTTP DELETE: ", this.httpClient.delete(this.urldelete + '/' + id, { observe: 'body' }).pipe(catchError(this.handleError<any>('deleteProduct'))));
     return this.httpClient.delete(this.urldelete+'/'+id,{observe:'body'}).pipe(catchError(this.handleError<any>('deleteProduct')))
   }
-
+  updateProduct(id: string, name: string, price: number, description: string): Observable<object> {
+    const data = { name: name, price: price, description: description };
+    console.log("src/app/services/product.service.ts, updateProduct(id: string, name: string, price: number, description: string)", data);
+    console.log("HTTP PUT: ",this.httpClient.put(this.urlput + '/' + id, { observe: 'body' }).pipe(catchError(this.handleError<any>('updateProduct'))));
+    return this.httpClient.put(this.urlput + '/' + id, { observe: 'body' }).pipe(catchError(this.handleError<any>('updateProduct')))
+  }
   private handleError<T>(operation = 'opearation',result?:T){
     return (error:any):Observable<T>=>{
       // TODO: send the error to remote logging infrastructure
